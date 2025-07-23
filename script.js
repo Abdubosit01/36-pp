554821929213fdb9ad0745c5768b414b79b26f98
Abdubosit01 <abdubosit0115@gmail.com>
2025-07-23 15:03:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+let a =prompt('isming')
+
+let c = prompt('parol')
+let b= prompt('sening id')
+
+
+
+
+
+
+
+let car = {
+    id:b,
+    ism:a,
+    parol:c
+    
+}
+
+
+console.log(car);
